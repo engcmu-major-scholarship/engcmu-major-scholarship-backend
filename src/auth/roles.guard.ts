ff3a7e066a579bd925aa0acfaa3a815bef770393
Yamami <yamami.fujita@gmail.com>
2024-12-18 14:57:08 +0700

    refactor authentication guards: remove unused AuthGuard, enhance JwtAuthGuard, and add RolesGuard with role-based access control

---
+++ src/auth/roles.guard.ts
@@ -0,0 +1,27 @@
+import { Reflector } from '@nestjs/core';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { ExecutionContext } from '@nestjs/common';
+import { Role } from './types/Role';
+import { ROLES_KEY } from 'src/decorators/roles.decorator';
+
+export class RolesGuard extends JwtAuthGuard {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const superCanActivate = super.canActivate(context);
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return superCanActivate;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    return (
+      requiredRoles.some((role) => user.roles?.includes(role)) &&
+      superCanActivate
+    );
+  }
+}
