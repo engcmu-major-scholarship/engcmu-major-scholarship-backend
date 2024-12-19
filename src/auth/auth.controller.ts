d583acbda960ac9d472f52bc886b82e7a41222fd
Yamami <yamami.fujita@gmail.com>
2024-12-19 17:49:43 +0700

    add public access decorator and implement role-based access control

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,16 +11,19 @@ import { Request as ExpressRequest } from 'express';
 import { SignupDTO } from './dtos/signup.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { SigninDTO } from './dtos/signin.dto';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('signin')
   async googleSignin(@Body() body: SigninDTO) {
     return this.authService.googleSignin(body.accessToken);
   }
 
+  @Public()
   @Post('signup')
   async googleSignup(@Body() body: SignupDTO) {
     return this.authService.googleSignup(body.accessToken, body.citizenId);
