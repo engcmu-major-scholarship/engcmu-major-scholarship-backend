d3a480b2189bc3e1755d6a80e5975ab32aac09bd
PremerX007 <bannawat_55120@outlook.com>
2025-01-04 03:33:43 +0700

    refactor application module to support semester filtering and improve query structure

--- src/application/application.controller.ts
+++ src/application/application.controller.ts
@@ -1,21 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { ApplicationService } from './application.service';
 import { Role } from 'src/auth/types/Role';
 import { Roles } from 'src/decorators/roles.decorator';
 
 @Controller('application')
 export class ApplicationController {
-  constructor(private readonly applicationService: ApplicationService) { }
+  constructor(private readonly applicationService: ApplicationService) {}
 
   @Roles(Role.ADMIN)
-  @Get(':year')
-  findbyYear(@Param('year') year: string) {
-    return this.applicationService.findbyYear(+year);
+  @Get(':year/:semester')
+  findbyYear(
+    @Param('year', ParseIntPipe) year: number,
+    @Param('semester', ParseIntPipe) semester: number,
+  ) {
+    return this.applicationService.findByYear(year, semester);
   }
 
   @Roles(Role.ADMIN)
   @Get('scholarship/:scholarId')
-  findApprovedStudent(@Param('scholarId') scholarId: string) {
-    return this.applicationService.findApprovedStudentByScholar(+scholarId);
+  findApprovedStudent(@Param('scholarId', ParseIntPipe) scholarId: number) {
+    return this.applicationService.findApprovedStudentByScholar(scholarId);
   }
 }
--- src/application/application.service.ts
+++ src/application/application.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
-import { Not, IsNull } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { Not, IsNull, LessThan, In } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Application } from 'src/models/application.entity';
@@ -9,89 +9,65 @@ export class ApplicationService {
   constructor(
     @InjectRepository(Application)
     private readonly applicationRepository: Repository<Application>,
-  ) { }
+  ) {}
 
-  async findbyYear(year: number) {
-    try {
-      const applications = await this.applicationRepository.find({
-        where: { year },
-        relations: ['student', 'scholarship'],
-        select: {
-          id: true,
-          student: { id: true, firstName: true, lastName: true },
-          scholarship: { name: true },
-        }
-      });
+  async findByYear(year: number, semester: number) {
+    const applications = await this.applicationRepository.find({
+      where: { year, semester },
+      relations: {
+        student: true,
+        scholarship: true,
+      },
+      select: {
+        id: true,
+        student: { id: true, firstName: true, lastName: true },
+        scholarship: { name: true },
+      },
+    });
 
-      if (!applications.length) {
-        throw new NotFoundException('No application found for this year');
-      }
+    const studentIds = applications.map((app) => app.student.id);
 
-      return applications.map((app) => ({
-        student_id: app.student.id,
-        firstname: app.student.firstName,
-        lastname: app.student.lastName,
-        scholar_name: app.scholarship.name,
-      }));
+    const previousApplications = await this.applicationRepository.find({
+      where: {
+        student: { id: In(studentIds) },
+        year: LessThan(year),
+      },
+      relations: {
+        student: true,
+      },
+      select: { student: { id: true } },
+    });
 
-      // Optimized Query
-      // return await this.applicationRepository
-      //   .createQueryBuilder('application')
-      //   .innerJoinAndSelect('application.student', 'student')
-      //   .innerJoinAndSelect('application.scholarship', 'scholarship')
-      //   .select([
-      //     'application.studentId AS student_id',
-      //     'student.firstName AS firstname',
-      //     'student.lastName AS lastname',
-      //     'scholarship.name AS scholar_name',
-      //   ])
-      //   .where('application.year = :year', { year })
-      //   .getRawMany();
+    const previousStudentIds = new Set(
+      previousApplications.map((app) => app.student.id),
+    );
 
-    } catch (error) {
-      if (error instanceof NotFoundException) { throw error; }
-      throw new InternalServerErrorException('Error in finding application by year');
-    }
+    return applications.map((app) => ({
+      studentId: app.student.id,
+      firstName: app.student.firstName,
+      lastName: app.student.lastName,
+      scholarName: app.scholarship.name,
+      isFirstTime: !previousStudentIds.has(app.student.id),
+    }));
   }
 
   async findApprovedStudentByScholar(scholarId: number) {
-    try {
-      const applications = await this.applicationRepository.find({
-        where: { scholarship: { id: scholarId }, adminApprovalTime: Not(IsNull()) },
-        relations: ['student'],
-        select: {
-          id: true,
-          student: { id: true, firstName: true, lastName: true },
-        }
-      });
+    const applications = await this.applicationRepository.find({
+      where: {
+        scholarship: { id: scholarId },
+        adminApprovalTime: Not(IsNull()),
+      },
+      relations: { student: true },
+      select: {
+        id: true,
+        student: { id: true, firstName: true, lastName: true },
+      },
+    });
 
-      if (!applications.length) {
-        throw new NotFoundException('No approved students found for this scholarship');
-      }
-
-      return applications.map((app) => ({
-        student_id: app.student.id,
-        firstname: app.student.firstName,
-        lastname: app.student.lastName,
-      }));
-
-      // Optimized Query
-      // return await this.applicationRepository
-      //   .createQueryBuilder('application')
-      //   .innerJoinAndSelect('application.student', 'student')
-      //   .select([
-      //     'application.studentId AS student_id',
-      //     'student.firstName AS firstname',
-      //     'student.lastName AS lastname',
-      //   ])
-      //   .where('application.scholarshipId = :scholarId', { scholarId })
-      //   .andWhere('application.adminApprovalTime IS NOT NULL')
-      //   .getRawMany();
-
-    } catch (error) {
-      if (error instanceof NotFoundException) { throw error; }
-      throw new InternalServerErrorException('Error in finding approved student for scholarship');
-    }
+    return applications.map((app) => ({
+      studentId: app.student.id,
+      firstName: app.student.firstName,
+      lastName: app.student.lastName,
+    }));
   }
 }
-
