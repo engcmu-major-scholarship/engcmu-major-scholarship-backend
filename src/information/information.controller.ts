0fa288072a0162327d0891d5c963990c8cb7504d
PremerX007 <bannawat_55120@outlook.com>
2025-02-11 03:25:23 +0700

    Add create and patch for information endpoint.

--- src/information/information.controller.ts
+++ src/information/information.controller.ts
@@ -6,18 +6,66 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFiles,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { InformationService } from './information.service';
 import { CreateInformationDto } from './dto/create-information.dto';
 import { UpdateInformationDto } from './dto/update-information.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiBodyOptions,
+  ApiConsumes,
+} from '@nestjs/swagger';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/auth/types/Role';
+import { FileFieldsByTypeInterceptor } from 'src/utils/interceptor/file-fields-by-type.interceptor';
+import { CreateInformationFilesDto } from './dto/create-information-files.dto';
+import { ParseFileFieldsPipe } from 'src/utils/pipe/parse-file-fields.pipe';
+import { UpdateInformationFilesDto } from './dto/update-information-files.dto';
+
+const apiBodyOptions: ApiBodyOptions = {
+  schema: {
+    type: 'object',
+    properties: {
+      name: { type: 'string' },
+      description: { type: 'string' },
+      informationDoc: { type: 'string', format: 'binary' },
+      published: { type: 'boolean' },
+    },
+  },
+};
 
 @Controller('information')
 export class InformationController {
   constructor(private readonly informationService: InformationService) {}
 
+  @ApiBearerAuth()
+  @ApiConsumes('multipart/form-data')
+  @ApiBody(apiBodyOptions)
+  @Roles(Role.ADMIN)
   @Post()
-  create(@Body() createInformationDto: CreateInformationDto) {
-    return this.informationService.create(createInformationDto);
+  @UseInterceptors(
+    FileFieldsByTypeInterceptor<CreateInformationFilesDto>({
+      informationDoc: { maxCount: 1 },
+    }),
+  )
+  create(
+    @Body() createInformationDto: CreateInformationDto,
+    @UploadedFiles(
+      new ParseFileFieldsPipe<CreateInformationFilesDto>({
+        informationDoc: {
+          type: 'application/pdf',
+          required: true,
+          itemCount: 1,
+        },
+      }),
+    )
+    files: CreateInformationFilesDto,
+  ) {
+    return this.informationService.create(createInformationDto, files);
   }
 
   @Get()
@@ -30,12 +78,30 @@ export class InformationController {
     return this.informationService.findOne(+id);
   }
 
+  @ApiBearerAuth()
+  @ApiConsumes('multipart/form-data')
+  @ApiBody(apiBodyOptions)
+  @Roles(Role.ADMIN)
   @Patch(':id')
+  @UseInterceptors(
+    FileFieldsByTypeInterceptor<CreateInformationFilesDto>({
+      informationDoc: { maxCount: 1 },
+    }),
+  )
   update(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body() updateInformationDto: UpdateInformationDto,
+    @UploadedFiles(
+      new ParseFileFieldsPipe<UpdateInformationFilesDto>({
+        informationDoc: {
+          type: 'application/pdf',
+          itemCount: 1,
+        },
+      }),
+    )
+    files: UpdateInformationFilesDto,
   ) {
-    return this.informationService.update(+id, updateInformationDto);
+    return this.informationService.update(+id, updateInformationDto, files);
   }
 
   @Delete(':id')
--- src/information/information.service.ts
+++ src/information/information.service.ts
@@ -1,11 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateInformationDto } from './dto/create-information.dto';
 import { UpdateInformationDto } from './dto/update-information.dto';
+import { S3Service } from 'src/s3/s3.service';
+import { Information } from 'src/models/information.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateInformationFilesDto } from './dto/create-information-files.dto';
+import { UpdateInformationFilesDto } from './dto/update-information-files.dto';
+import { isNotEmptyObject } from 'class-validator';
 
 @Injectable()
 export class InformationService {
-  create(createInformationDto: CreateInformationDto) {
-    return 'This action adds a new information';
+  constructor(
+    private readonly s3Service: S3Service,
+    @InjectRepository(Information)
+    private readonly informationRepository: Repository<Information>,
+  ) {}
+
+  async create(
+    createInformationDto: CreateInformationDto,
+    files: CreateInformationFilesDto,
+  ) {
+    if (
+      await this.informationRepository.existsBy({
+        name: createInformationDto.name,
+      })
+    ) {
+      throw new UnprocessableEntityException('Information already exists');
+    }
+
+    const informationDocKey = createInformationDto.name;
+
+    const information = this.informationRepository.create({
+      name: createInformationDto.name,
+      description: createInformationDto.description,
+      detailDocument: informationDocKey,
+      published: createInformationDto.published,
+    });
+    await this.informationRepository.save(information);
+
+    await this.s3Service.uploadFile(
+      'major-scholar-information-doc',
+      informationDocKey,
+      files.informationDoc[0].buffer,
+      files.informationDoc[0].mimetype,
+    );
   }
 
   findAll() {
@@ -16,8 +59,38 @@ export class InformationService {
     return `This action returns a #${id} information`;
   }
 
-  update(id: number, updateInformationDto: UpdateInformationDto) {
-    return `This action updates a #${id} information`;
+  async update(
+    id: number,
+    updateInformationDto: UpdateInformationDto,
+    files: UpdateInformationFilesDto,
+  ) {
+    const information = await this.informationRepository.findOneBy({
+      id,
+    });
+
+    if (!information) {
+      throw new NotFoundException('Information not found');
+    }
+
+    const informationDocKey = information.detailDocument;
+    if (isNotEmptyObject(updateInformationDto)) {
+      await this.informationRepository.update(id, {
+        name: updateInformationDto.name,
+        description: updateInformationDto.description,
+        published: updateInformationDto.published,
+      });
+    } else {
+      if (!files) throw new UnprocessableEntityException('No data to update');
+    }
+
+    if (files.informationDoc) {
+      await this.s3Service.uploadFile(
+        'major-scholar-information-doc',
+        informationDocKey,
+        files.informationDoc[0].buffer,
+        files.informationDoc[0].mimetype,
+      );
+    }
   }
 
   remove(id: number) {
